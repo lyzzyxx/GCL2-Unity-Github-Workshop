1c1d63c7b5aa8de937bfeaa10c8c990aae5753d2
CaffOD <lee.mitchel.mh@gmail.com>
2026-04-29 11:07:18 +0800

    Mitchel Add Player Sprite, Mystery Box Game Object and Script

---
+++ Assets/Scripts/MysteryBoxMitchel.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MysteryBoxMitchel : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Debug.Log("It's a box, there's stuff in it");
+        }
+    }
+}
